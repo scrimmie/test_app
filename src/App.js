30f7d7c0bba91cc0ff4be500e3b0555fe6226000
David D'Apice <48042051+scrimmie@users.noreply.github.com>
2021-06-16 17:29:27 -0400

    Added in check for loading
    Added background color and altered the loading flow.

--- src/App.js
+++ src/App.js
@@ -16,8 +16,10 @@ function App() {
   }, [count])
 
   return (
-    <div className="flex flex-col w-screen h-screen">
-      <div className="flex flex-col mx-auto my-auto bg-local bg-gray-500 rounded px-5 py-5 w-1/2">
+    <div className="flex flex-col w-screen h-screen bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500">
+      <div className="flex flex-col mx-auto my-auto bg-local bg-gray-500 rounded-lg px-5 py-5 w-1/2">
+      {user ? 
+      <>
       <img src={logo} className="mx-auto max-w-sm self-center justify-self-center" alt="logo" />
       <p className="text-center font-sans text-8xl"> User Checker </p>
       <p className="text-center font-sans text-3xl"> User: {count} </p>
@@ -28,6 +30,10 @@ function App() {
         <p className="text-center font-sans text-3xl text-red-500	"> Completed: {user.completed.toString()} </p>
       }
       <button onClick={() => {setCount(count + 1)}} className="mx-auto mt-10 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Next User</button>
+      </>
+      :
+      <svg class="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24"></svg>
+      }
       </div>
     </div>
   );
